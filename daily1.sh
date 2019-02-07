7d338ac047e3b0d9d27e02525352577d8a9628df
hybridth1 <47426946+hybridth1@users.noreply.github.com>
2019-02-07 23:51:55 +0700

    Add files via upload

---
+++ daily1.sh
@@ -0,0 +1,24 @@
+echo "1:Sedih Ga sih Waktu Lagi Sayang-Sayangnya Tiba-tiba Di Tinggal"
+sleep 4	
+echo "2:Lebih Parahnya Pas Waktu Ngedate Dia Malah Sibuk Chatingan:("
+sleep 4
+echo "3:Apalagi Waktu Chatingan:( Chat kita Dibalesnya Lama Sedangkan Chat Dia Fash Respon:)"
+sleep 4
+echo "4:Dear Ceue Hargailah Couolu Selagi Dia Masih Menyayangi Lu"
+sleep 5
+echo
+echo
+echo
+echo
+echo "Selagi Lu Masih Mempunyai Couo Yang Baik Dan Bisa Jaga lu"
+sleep 3
+echo "Jangan Terlalu Tergoda Dengan Harta"
+sleep 4
+echo "karna Kebahagian Bukan Soal Harta"
+sleep 1
+echo "Tetapi Soal Apa Yang sudah Kalian Perjuangkan tanpa harta"
+echo
+echo
+echo
+sleep 3
+echo "CREATED BY DENN"
