97991073bcac2402890ed7161c9388892daa524c
hybridth1 <47426946+hybridth1@users.noreply.github.com>
2019-02-09 14:29:56 +0700

    Add files via upload

---
+++ daily2.sh
@@ -0,0 +1,17 @@
+echo $GREEN "jika Kau sedang mencintai seseorang maka perjuangkanlah"
+sleep 2
+echo "Dan jika kau tidak mencintainya tinggalkanlah"
+sleep 2
+echo "Kenapa harus ditinggalkan?"
+sleep 1
+echo "Karena sama saja kau menaruh Harapan kepadanya tetapi kau tidak mengharapkannya"
+sleep 2
+echo "Jangan Pernah Mempermainkan Soal Percintaan"
+sleep 2
+echo "Karna Tiada Cinta Kau tidak akan bisa idup"
+echo
+echo
+echo
+echo "Created By:Hybridth"
+echo "Good_Luck"
+echo "See You Again"
